0211e3fe240719f804995c5d0dab48830bf22e54
Oshan Avishka <oshanavishka06@gmail.com>
2025-12-06 16:51:52 +0530

    feat: implement home page with HeroSection

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,21 @@
 'use client';
 
 import { HeroSection } from '@/components/home/hero-section';
-import { RecentNewsSection } from '@/components/home/recent-news-section';
+//import { RecentNewsSection } from '@/components/home/recent-news-section';
 //import {PopularSubjectsSection} from '@/components/home/popular-subjects-section';
-import { ContributorsSection } from '@/components/home/contributors-section';
+//import { ContributorsSection } from '@/components/home/contributors-section';
 //import {LatestUploadsSection} from '@/components/home/latest-uploads-section';
-import { WelcomeModal } from '@/components/home/welcome-modal';
+//import { WelcomeModal } from '@/components/home/welcome-modal';
 
 export default function Home() {
     return (
         <div className='flex flex-col w-full min-h-screen'>
-            <WelcomeModal />
+            {/* <WelcomeModal /> */}
             <HeroSection />
-            <RecentNewsSection />
+            {/* <RecentNewsSection /> */}
             {/* <PopularSubjectsSection/> */}
             {/* <LatestUploadsSection/> */}
-            <ContributorsSection />
+            {/* <ContributorsSection /> */}
         </div>
     );
 }
